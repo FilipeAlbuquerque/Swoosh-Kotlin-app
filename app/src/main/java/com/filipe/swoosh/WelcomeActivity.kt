82f65235ef5cf6f3dc2cce6ca6976218b37e1715
FilipeAlbuquerque <filipefragosoti@gmail.com>
2019-06-17 10:20:44 -0300

    Implementing navigation through activity from get started button

--- app/src/main/java/com/filipe/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/filipe/swoosh/WelcomeActivity.kt
@@ -1,12 +1,19 @@
 package com.filipe.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            val intentLeague = Intent(this, LeagueActivity::class.java)
+            startActivity(intentLeague)
+        }
     }
 }
