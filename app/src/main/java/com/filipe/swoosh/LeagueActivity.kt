c61d4fd0e252f1f2758bfbd9b10f3e438627fbbe
FilipeAlbuquerque <filipefragosoti@gmail.com>
2019-06-17 11:44:28 -0300

    Skill activity and function to the nex button in league activity added

--- app/src/main/java/com/filipe/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/filipe/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package com.filipe.swoosh
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+    }
+
+    fun leagueNextClicked(view: View){
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
---
+++ app/src/main/java/com/filipe/swoosh/SkillActivity.kt
@@ -0,0 +1,12 @@
+package com.filipe.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
